2cf385f03539cc8d4f8a4b2f7aa7b47f9ad82d11
Evan <ewhite755@gmail.com>
2020-01-26 06:43:56 -0500

    movement+nodes added

--- Pac-Man/Assets/Scripts/Node.cs
+++ Pac-Man/Assets/Scripts/Node.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Node : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public Node[] neighbors;
+    public Vector2[] validDirections;
+
+	void Start () 
+    {
+        validDirections = new Vector2[neighbors.Length];
+
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            Node neighbor = neighbors[i];
+            Vector2 tempVector = neighbor.transform.localPosition - transform.localPosition;
+
+            validDirections[i] = tempVector.normalized;
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
